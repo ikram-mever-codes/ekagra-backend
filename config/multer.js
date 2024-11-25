96730826b80163dd90be19124c922a23eec1c3d7
ikram-mever-codes <ikram0pakitan@gmail.com>
2024-11-25 11:50:22 +0500

    final

--- config/multer.js
+++ config/multer.js
@@ -25,7 +25,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB file size limit
+  limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter,
 }).single("file");
 
